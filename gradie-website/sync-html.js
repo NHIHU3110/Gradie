b610082d30965c9f0a9121acf42430de1d78adf2
NHIHUYNH3110 <nhiht234112e@st.uel.edu.vn>
2026-05-31 12:22:43 +0700

    feat: replace python sync script with a native Node.js sync-html.js script to ensure pure JS environment

---
+++ gradie-website/sync-html.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+
+const rootDir = '/Users/huynhthaonhi/Downloads/Gradie/gradie-website';
+const htmlDir = path.join(rootDir, 'html');
+
+if (!fs.existsSync(htmlDir)) {
+    fs.mkdirSync(htmlDir, { recursive: true });
+}
+
+// Find all HTML files in the root dir
+const files = fs.readdirSync(rootDir);
+const htmlFiles = files.filter(f => f.endsWith('.html'));
+
+console.log(`Syncing ${htmlFiles.length} HTML files from root to html/ folder...`);
+
+htmlFiles.forEach(filename => {
+    const rootPath = path.join(rootDir, filename);
+    const destPath = path.join(htmlDir, filename);
+    
+    let content = fs.readFileSync(rootPath, 'utf8');
+    
+    // Replace resource paths to point one level up
+    // Match href="css/..., href='css/..., src="js/..., src="images/...
+    content = content.replace(/href=["']css\//g, 'href="../css/');
+    content = content.replace(/src=["']js\//g, 'src="../js/');
+    content = content.replace(/src=["']images\//g, 'src="../images/');
+    
+    // Write to html/ folder
+    fs.writeFileSync(destPath, content, 'utf8');
+});
+
+console.log("Sync completed successfully via Node.js!");
